d42c5d8f09cac4310d9efb71ad4d69054e1589dd
VivianChu <tchu@ucsd.edu>
2016-02-04 14:26:03 -0800

    Update deploy config. DMR-73

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,29 +8,42 @@ set :scm, :git
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'pids', 'node_modules')
 
 set :default_env, fetch(:default_env, {}).merge('PATH' => "/pub/deploybot/current/node_modules/.bin:/pub/deploybot/current/node_modules/hubot/node_modules/.bin:$PATH")
-namespace :deploy do
 
-  desc "Install npm"
+namespace :deploy do
+  desc "Sets up the log file, then sources EnvVars & starts Hubot"
+  task :start do
+    log_file = "#{shared_path}/log/hubot.log"
+    # If we've got a log file already, mark that a deployment occurred
+    on roles(:app) do
+      execute "if [ -e #{log_file} ]; then echo \"\n\nDeployment #{release_timestamp}\n\" >> #{log_file}; fi"
+      # Start Hubot!
+      execute "source /home/deploy/.bashrc && \
+        cd #{release_path} && \
+        forever start -p #{shared_path} --pidFile #{shared_path}/pids/hubot.pid -a -l #{shared_path}/log/hubot.log -c coffee node_modules/.bin/hubot -a slack -d"
+    end
+  end
+
+  desc "Stop Hubot"
+  task :stop do
+    on roles(:app) do
+      test "source /home/deploy/.bashrc && \
+        cd /pub/deploybot/current && \
+        forever stop $(cat #{shared_path}/pids/hubot.pid)"
+    end
+  end
+
+  desc "Install necessary Node modules, then move them to the correct path"
   task :npm_install do
     on roles(:app) do
       execute "cd #{release_path} && npm install"
     end
   end
-  
-  desc 'Restart Hubot'
-  task :restart do
-    log_file = "#{shared_path}/log/hubot.log"
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute "if [ -e #{log_file} ]; then echo \"\n\nDeploy #{release_timestamp}\n\" >> #{log_file}; fi"      
-      # Stop Hubot
-      test "source /home/deploy/.bashrc && cd /pub/deploybot/current && forever stop $(cat #{shared_path}/pids/hubot.pid)"       
-      # Start Hubot
-      execute "source /home/deploy/.bashrc && cd #{release_path} && \
-        forever start -p #{shared_path} --pidFile #{shared_path}/pids/hubot.pid -a -l #{shared_path}/log/hubot.log -c coffee node_modules/.bin/hubot -a slack -d"
-    end
-  end
 
-  after :finishing, 'deploy:npm_install'
-  after :finishing, 'deploy:restart'
+  desc "Base task to restart Hubot after a deployment if he's already running"
+  task :restart do
+    invoke "deploy:stop"
+    invoke "deploy:start"
+  end
 end
+after "deploy:published", "deploy:restart"
+before "deploy:updated", "deploy:npm_install"
